b7b71bc745d6c5c62c267c9121a6bf50fa7a1341
Nigliox <fm@ct-s.ch>
2019-01-13 11:38:08 +0100

    Forks Class added with Put and Get Methodes with Lock

---
+++ TestatMFPhilosoph/TestatMFPhilosoph/Forks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestatMFPhilosoph
+{
+    // This class will provide forks to the philosophers and monitor if the fork is in use or not
+
+
+    class Forks
+    {
+        private readonly bool[] fork = new bool[5];
+
+        // Attempt to pick up the forks with the designated nubers
+        public void Get(int leftFork, int rightFork)
+        {
+            lock (this)
+            {
+                while (fork[leftFork] || fork[rightFork])
+                {
+                    Monitor.Wait(this);
+                }
+
+                fork[leftFork] = true;
+                fork[rightFork] = true;
+            }
+        }
+
+        public void Put(int leftFork, int rightFork)
+        {
+            lock (this)
+            {
+                fork[leftFork] = false;
+                fork[rightFork] = false;
+                Monitor.PulseAll(this);
+            }
+        }
+    }
+}
